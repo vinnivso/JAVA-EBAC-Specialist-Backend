7240e8813409fd3b78fa88b240da8eafc25d7d66
vinnivso <vinnivsob@gmail.com>
2024-01-05 10:02:12 -0300

    General Update

---
+++ modulo13-Interfaces-Heranca/modulo-13/src/main/java/org/vinicius/abstratas/Empregado.java
@@ -0,0 +1,7 @@
+package org.vinicius.abstratas;
+
+public class Empregado {
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+}
--- modulo13-Interfaces-Heranca/modulo-13/src/main/java/org/vinicius/interfaces/ICaneta.java
+++ modulo13-Interfaces-Heranca/modulo-13/src/main/java/org/vinicius/interfaces/ICaneta.java
@@ -3,4 +3,9 @@ package org.vinicius.interfaces;
 public interface ICaneta {
     public void escrever(String texto);
     public String getCor();
+
+    //Partindo do JAVA 8, foi estabelecido a criação de métodos comuns seguindo a forma abaixo
+    default void escreverComumATodas() {
+        System.out.println("Escrita igual a todas!");
+    }
 }
