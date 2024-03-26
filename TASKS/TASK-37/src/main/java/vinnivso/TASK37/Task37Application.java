8d75e4d88bc9dc25cc9d4c68871ea888442cdf1b
Vinicius Oliveira <vinnivsob@gmail.com>
2024-03-26 20:37:19 -0300

    Develop: Module-37

---
+++ TASKS/TASK-37/src/main/java/vinnivso/TASK37/Task37Application.java
@@ -0,0 +1,13 @@
+package vinnivso.TASK37;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task37Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task37Application.class, args);
+	}
+
+}
---
+++ TASKS/TASK-37/src/test/java/vinnivso/TASK37/Task37ApplicationTests.java
@@ -0,0 +1,13 @@
+package vinnivso.TASK37;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task37ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
